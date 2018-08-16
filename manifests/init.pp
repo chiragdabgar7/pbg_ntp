6917471e2c925c3329454ccf14aec1b9d0f78889
chiragdabgar7 <chiragdabgar7@gmail.com>
2018-08-16 14:35:14 +0000

    Add module manifesr and config files

---
+++ manifests/init.pp
@@ -0,0 +1,15 @@
+#Manager NTP
+class pbg_ntp {
+  ensure_packages(['ntp'])
+  
+  file { '/etc/ntp.conf':
+    source 	=> 'puppet:///modules/pbg_ntp/ntp.conf',
+    notify	=> Service['ntp'],
+    require	=> Package['np'],
+  }
+
+  service { 'ntp':
+    ensure	=> running,
+    enable	=> true,
+  }
+}
